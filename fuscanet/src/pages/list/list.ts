1f542b6bd14d63746cbf7ec3dc1d67e9b22f66e5
codigo <omargonzaleztecheira@gmail.com>
2017-09-14 20:23:24 -0300

    calendario agregando eventos

--- fuscanet/src/pages/list/list.ts
+++ fuscanet/src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController, AlertController } from 'ionic-angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-list',
@@ -15,7 +16,7 @@ export class ListPage {
     currentDate: this.selectedDate
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController, private alertCtrl: AlertController) {
     
   }
   onViewTitleChanged(title){
@@ -26,10 +27,33 @@ export class ListPage {
     console.log(ev.selectedTime);
   }
   onEventSelected(event){
-
+    let start = moment(event.startTime).format('LLLL');
+    let end = moment(event.endTime).format('LLLL');
+    let alert = this.alertCtrl.create({
+      title: ''+event.title,
+      subTitle: 'Desde: '+start+ '<br>Hasta: '+end,
+      buttons: ['OK']
+    });
+    alert.present();
   }
   addEvent(){
-    
+    let modal = this.modalCtrl.create('EventModalPage', {selectedDate: this.selectedDate});
+    modal.present();
+    modal.onDidDismiss(data => {
+      if (data) {
+        let eventData = data;
+ 
+        eventData.startTime = new Date(data.startTime);
+        eventData.endTime = new Date(data.endTime);
+ 
+        let events = this.eventSource;
+        events.push(eventData);
+        this.eventSource = [];
+        setTimeout(() => {
+          this.eventSource = events;
+        });
+      }
+    });
   }
 
 }
