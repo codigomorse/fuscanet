42120e016422abb95d7fb2ce044249eeb6907603
codigo <omargonzaleztecheira@gmail.com>
2017-09-14 21:04:17 -0300

    connection con base de productos

--- fuscanet/src/pages/catalogo/catalogo.ts
+++ fuscanet/src/pages/catalogo/catalogo.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseObjectObservable  } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-catalogo',
   templateUrl: 'catalogo.html'
 })
 export class Catalogo {
+  user={};
+  productos=[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alertCtrl: AlertController,private afDb: AngularFireDatabase,private afAuth:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
   }
-
+  ionViewDidLoad() {
+    this.afAuth.authState.subscribe(data => {
+      this.user = data;
+      //console.log(this.user);  
+      this.afDb.object(`/product/`).subscribe(_data => {
+          this.productos = _data;
+          console.log(this.productos);
+      });  
+     });
+  }
 }
