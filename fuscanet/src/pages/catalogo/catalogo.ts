d9a557fe2af24dfd00ff307154b62a7e6b743839
codigo <omargonzaleztecheira@gmail.com>
2017-09-14 22:20:15 -0300

    lista de los 3mil productos

--- fuscanet/src/pages/catalogo/catalogo.ts
+++ fuscanet/src/pages/catalogo/catalogo.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseObjectObservable  } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable  } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'page-catalogo',
@@ -9,18 +10,16 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class Catalogo {
   user={};
-  productos=[];
+  productos$: FirebaseListObservable<Product[]>;
 
   constructor(public alertCtrl: AlertController,private afDb: AngularFireDatabase,private afAuth:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
-  }
-  ionViewDidLoad() {
     this.afAuth.authState.subscribe(data => {
       this.user = data;
       //console.log(this.user);  
-      this.afDb.object(`/product/`).subscribe(_data => {
-          this.productos = _data;
-          console.log(this.productos);
-      });  
-     });
+      this.productos$ = this.afDb.list('product');
+      this.productos$.subscribe(data => console.log(data));
+
+     }); 
   }
+
 }
