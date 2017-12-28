fba80d0aa565d5d20bfa54e029a0e1149dc3fb5d
codigo <omargonzaleztecheira@gmail.com>
2017-12-28 18:57:55 -0300

    vademecum a full!!!

--- fuscanet/src/pages/labdetails/labdetails.ts
+++ fuscanet/src/pages/labdetails/labdetails.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController, ViewController } from 'ionic-angular';
+import { NavController, NavParams, AlertController, ViewController, ModalController } from 'ionic-angular';
 import { Product } from '../../models/product';
+import { Itemdetails } from '../itemdetails/itemdetails';
 
 @Component({
   selector: 'page-labdetails',
@@ -12,18 +13,24 @@ export class Labdetails {
   datos;
   origProd:any;
 
-  constructor(public navCtrl: NavController,public viewCtrl:ViewController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,public viewCtrl:ViewController, public navParams: NavParams,private modalCtrl:ModalController) {
     this.lab=this.navParams.get('lab');
-    console.log(this.lab);
-    console.log(this.lab.$key);
+    //console.log(this.lab);
+    //console.log(this.lab.$key);
     this.titulo=this.lab.$key;
     this.origProd=this.navParams.get('origProd');
-    console.log(this.origProd);
+    //console.log(this.origProd);
     this.origProd=this.origProd.filter((product)=>{
       //console.log(product.Laboratorio);
       return product.Laboratorio.startsWith(this.lab.$key);
     });
-    console.log(this.origProd);
+    //console.log(this.origProd);
+  }
+  productClick(product){
+    let modal = this.modalCtrl.create(Itemdetails,  {'product': product});
+    //modal.onDidDismiss((data) => {console.log(data)});
+    modal.present();
+    
   }
 
   closeModal(){
