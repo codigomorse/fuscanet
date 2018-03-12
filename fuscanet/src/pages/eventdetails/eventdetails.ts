a82085cb3542ed532184cb6e9ed58bb86bfdea78
codigo <omargonzaleztecheira@gmail.com>
2018-03-12 20:38:37 -0300

    guardar link click en noticias detalle

--- fuscanet/src/pages/eventdetails/eventdetails.ts
+++ fuscanet/src/pages/eventdetails/eventdetails.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController, ViewController } from 'ionic-angular';
 import { Event } from '../../models/event';
+import firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Profile } from '../../models/profile';
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-eventdetails',
@@ -9,7 +13,7 @@ import { Event } from '../../models/event';
 export class Eventdetails {
   titulo;
   evento: Event;
-  constructor(public viewCtrl:ViewController,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public viewCtrl:ViewController,public navCtrl: NavController, public navParams: NavParams,private afDb: AngularFireDatabase,private afAuth:AngularFireAuth) {
     this.evento=this.navParams.get('event');
     //console.log(this.evento);
   }
@@ -17,5 +21,12 @@ export class Eventdetails {
 
   closeModal(){
     this.viewCtrl.dismiss();
+  }
+  goLink(event){
+    console.log(event);
+    this.afAuth.authState.take(1).subscribe(auth => {
+      //console.log(auth);  
+      this.afDb.database.ref(`noticiaLike/${event.id}`).push(auth.uid);
+    });
   }
 }
